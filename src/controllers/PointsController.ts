8c7b665a15edbcf10023549a2da03d43e4d462ca
Leonardo Viana <leonardo.viana@meep-app.com>
2020-06-05 15:36:07 -0300

    Add some atributes

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -48,25 +48,25 @@ class PointsController {
       city,
       uf,
       items,
-      // street,
-      // number,
-      // neighborhood,
+      street,
+      number,
+      neighborhood,
     } = req.body;
 
     const trx = await knex.transaction();
 
     const point = {
       name,
-      image,
+      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       email,
       whatsapp,
       latitude,
       longitude,
       city,
       uf,
-      // street,
-      // number,
-      // neighborhood,
+      street,
+      number,
+      neighborhood,
     };
 
     const insertedIds = await trx("points").insert(point);
--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -11,9 +11,9 @@ export async function up(knex: Knex){
     table.decimal('longitude').notNullable();
     table.string('city').notNullable();
     table.string('uf', 2).notNullable();
-    // table.string('street').notNullable();
-    // table.string('number').notNullable();
-    // table.string('neighborhood').notNullable();
+    table.string('street').notNullable();
+    table.string('number').notNullable();
+    table.string('neighborhood').notNullable();
   });
 
 }
